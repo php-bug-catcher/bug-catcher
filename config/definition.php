2a2a8dd03c931e842a02f247721b2564e36239c5
tito10047 <jozef@mostka.sk>
2024-08-19 07:18:52 +0200

    fixed config

--- config/definition.php
+++ config/definition.php
@@ -16,34 +16,43 @@ return static function (DefinitionConfigurator $definition): void {
 		->scalarNode("app_name")->defaultValue("BugCatcher")->end()
 		->booleanNode("clear_stacktrace_on_fixed")->defaultValue(true)->end()
 		->arrayNode("dashboard_components")
-		->prototype('scalar')->end()
 		->defaultValue([
 			"StatusList",
 			"LogList",
-		])->end()
+		])
+		->prototype('scalar')
+		->end()
+		->end()
 		->arrayNode("notifier_components")
-		->prototype('scalar')->end()
 		->defaultValue([
 			"Project error count" => "project-error-count",
 			"Same error count"    => "same-error-count",
-		])->end()
+		])
+		->prototype('scalar')->end()->end()
 		->arrayNode("dashboard_list_items")
-		->prototype('scalar')->end()
 		->defaultValue([
 			RecordLog::class,
 			RecordLogTrace::class,
-		])->end()
+		])
+		->prototype('scalar')->end()->end()
 		->arrayNode("status_list_components")
-		->prototype('scalar')->end()
 		->defaultValue([
 				"ProjectStatus",
 				"LogCount",
 				"LogSparkLine",
 				"WarningSound",
 			]
-		)->end()
+		)
+		->prototype('scalar')->end()->end()
+		->arrayNode("collectors")
+		->defaultValue([
+			'http',
+			'messenger',
+		])
+		->prototype('scalar')
+		->end()
+		->end()
 		->arrayNode("detail_components")
-		->prototype('scalar')->end()
 		->defaultValue([
 			RecordLogTrace::class => [
 				'Detail:Header',
@@ -56,22 +65,21 @@ return static function (DefinitionConfigurator $definition): void {
 				'Detail:Title',
 				'Detail:HistoryList',
 			],
-		])->end()
-		->arrayNode("collectors")
-		->prototype('scalar')->end()
-		->defaultValue([
-			'http',
-			'messenger',
-		])->end()
+		])
+		->prototype('array')
+		->children()
+		->arrayNode("components")
+		->prototype('scalar')->end()->end()->end()->end()
+		->end()
 		->arrayNode("roles")
-		->prototype('scalar')->end()
 		->defaultValue([
 			"Admin"     => 'ROLE_ADMIN',
 			"Developer" => 'ROLE_DEVELOPER',
 			"User"      => 'ROLE_USER',
 			"Customer"  => 'ROLE_CUSTOMER',
-		])->end()
-            ->end()
+		])
+		->prototype('scalar')->end()->end()
+		->end()
         ->end()
     ;
 };
